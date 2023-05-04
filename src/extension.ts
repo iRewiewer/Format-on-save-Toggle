72b23c5d964b89c53052e9076f4c99dc6c2cdde6
iRewiewer <irewiewer@gmail.com>
2023-05-05 00:59:25 +0300

    implemented basic desired functionality

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,24 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vs from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
+export function activate(context: vs.ExtensionContext) {
 	console.log('Congratulations, your extension "formatonactivitybar" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('formatonactivitybar.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from FormatOnActivityBar!');
+	let disposable = vs.commands.registerCommand('formatonactivitybar.formatonsave', () => {
+		let config = vs.workspace.getConfiguration();
+		let configJSON = JSON.parse(JSON.stringify(config));
+		const editorSettings = configJSON.editor;
+
+		if (vs.workspace.getConfiguration().get("editor.formatOnSave")) {
+			vs.workspace.getConfiguration().update("editor.formatOnSave", false, true);
+			vs.window.showInformationMessage('Format on save has been [Disabled].');
+		}
+		else {
+			vs.workspace.getConfiguration().update("editor.formatOnSave", true, true);
+			vs.window.showInformationMessage('Format on save has been [Enabled].');
+		}
 	});
 
 	context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
