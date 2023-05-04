9bc9776782e768b40633b2aaeeaaf79f97e746e2
iRewiewer <irewiewer@gmail.com>
2023-05-05 02:59:04 +0300

    switched from activity bar to status bar

--- src/extension.ts
+++ src/extension.ts
@@ -1,24 +1,31 @@
+import { start } from 'repl';
 import * as vs from 'vscode';
 
-export function activate(context: vs.ExtensionContext) {
-	console.log('Congratulations, your extension "formatonactivitybar" is now active!');
+let app: vs.StatusBarItem;
+let startupText = vs.workspace.getConfiguration().get("editor.formatOnSave") ? 'FoS: Enabled' : 'FoS: Disabled';
 
-	let disposable = vs.commands.registerCommand('formatonactivitybar.formatonsave', () => {
-		let config = vs.workspace.getConfiguration();
-		let configJSON = JSON.parse(JSON.stringify(config));
-		const editorSettings = configJSON.editor;
+export function activate({ subscriptions }: vs.ExtensionContext) {
+	console.log('Started correctly.');
 
+	const commandId = "formatonsave.toggle";
+	subscriptions.push(vs.commands.registerCommand(commandId, () => {
 		if (vs.workspace.getConfiguration().get("editor.formatOnSave")) {
 			vs.workspace.getConfiguration().update("editor.formatOnSave", false, true);
-			vs.window.showInformationMessage('Format on save has been [Disabled].');
+			vs.window.showInformationMessage('Format on save has been Disabled.');
+			app.text = 'FoS: Disabled';
 		}
 		else {
 			vs.workspace.getConfiguration().update("editor.formatOnSave", true, true);
-			vs.window.showInformationMessage('Format on save has been [Enabled].');
+			vs.window.showInformationMessage('Format on save has been Enabled.');
+			app.text = 'FoS: Enabled';
 		}
-	});
+	}));
 
-	context.subscriptions.push(disposable);
+	app = vs.window.createStatusBarItem(vs.StatusBarAlignment.Right);
+	app.command = commandId;
+	app.text = startupText;
+	app.show();
+	subscriptions.push(app);
 }
 
 export function deactivate() { }
